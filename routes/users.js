86c9ae5f26dd30500ecedfbbbc3f0dc66522d616
Kush Guglani <kushguglani5@gmail.com>
2023-08-25 21:06:59 +0530

    signup initial api

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,50 @@
 const express = require('express');
-
+const UserModel = require('../models/user')
 
 
 const routes = express.Router();
 
-
-// app.post('');
-
+// duumy api for testing
 routes.get('/dummyapi', (req, res) => {
     res.send("dummy api working");
+})
+
+
+// user login api
+
+routes.get('/login', (req, res) => {
+    // validate user req body (username and password);
+    console.log(req.body);
+    if (!req.body.userName) {
+        res.status(400).send("username can not be empty");
+    } else if (!req.body.password) {
+        res.status(400).send("password can not be empty");
+    } else {
+        // validate from mongodb
+    }
+})
+
+routes.post('/signup', (req, res) => {
+    // validate user req body (username and password);
+    console.log(req.body);
+    if (!req.body.name) {
+        res.status(400).send("name can not be empty");
+    }
+    else if (!req.body.userName) {
+        res.status(400).send("username can not be empty");
+    } else if (!req.body.password) {
+        res.status(400).send("password can not be empty");
+    } else {
+        const newUser = new UserModel({
+            name:req.body.name,
+            userName:req.body.userName,
+            password:req.body.password,
+        })
+        newUser.save().then(user=>{
+            console.log(user);
+            res.send(user)
+        })
+    }
 })
 
 module.exports = routes;
